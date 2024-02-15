6162bf4297b2c58597d019cd45a498390a146936
Alejandro Ramos <lotusotho11@gmail.com>
2024-02-15 13:00:42 +0100

    Changed extension of non react scripts/order code

---
+++ src/Phrases.ts
@@ -0,0 +1,11 @@
+export const Phrases: string[] = [
+    "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    "You can observe a lot just by watching.",
+    "A house divided against itself cannot stand.",
+    "Be the chief but never the lord.",
+    "Life is a learning experience, only if you learn.",
+    "Well begun is half done.",
+    "Peace comes from within. Do not seek it without.",
+    "We can only learn to love by loving.",
+    "You'll see it when you believe it."
+  ]
