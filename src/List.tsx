8d2f550373fccac6218c1b8ab7c094f2ab5319b2
Alejandro Ramos <lotusotho11@gmail.com>
2024-02-15 17:00:54 +0100

    Create Functions.tsx

--- src/List.tsx
+++ src/List.tsx
@@ -1,13 +1,4 @@
-import * as React from "react";
-import { IList } from "./Interfaces";
-import DOMPurify from 'dompurify';
-
-
-const textArr: string[] = [];
-
-const listText: IList = {
-    text: ''
-}
+import { CreateStructureList, handleSumbit, DeleteListElementButton } from './Functions';
 
 export function List() {
     return (
@@ -27,75 +18,4 @@ export function List() {
             </form>
         </>
     )
-}
-
-function handleSumbit(event: React.SyntheticEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-
-    const form = event.currentTarget;
-
-    const formElements = form.elements as typeof form.elements & {
-        textInput: HTMLInputElement,
-    }
-
-    if (formElements.textInput.value != '') {
-        listText.text = formElements.textInput.value;
-
-        CreateListElement();
-
-        console.log(textArr)
-
-        formElements.textInput.value = '';
-    }
-}
-
-function CreateStructureList() {
-    return (
-        <div className="list-container-frame" id="wholeList" hidden>
-            <table>
-                <tbody id="textList" className="dataRow">
-                    
-                </tbody>
-            </table>
-        </div>
-    )
-}
-
-function UpdateArrayList(){
-    DOMPurify.sanitize(document.getElementById("textList")!.innerHTML = textArr.join().replace(/,/gm, '') + '\n');
-
-    if(textArr.length === 0){
-        document.getElementById("wholeList")!.hidden = true;
-        document.getElementById('btnDeleteLine')!.hidden = true;
-    } else {
-        document.getElementById("wholeList")!.hidden = false;
-        document.getElementById('btnDeleteLine')!.hidden = false;
-    }
-
-}
-
-function CreateListElement() {
-    const lineFormat: string = `<tr><td>${listText.text}</td></tr>`;
-
-    if(listText.text.length > 44){
-        alert("Try something smaller...");
-    } else if(listText.text.includes('<script>') || listText.text.includes('</script>')) {
-        alert("Yeaaaah, that's not going to work...");
-    } else {
-        textArr.push(lineFormat);
-    }
-
-    UpdateArrayList();
-}
-
-function DeleteListElementButton(event: React.SyntheticEvent<HTMLFormElement>) {
-    event.preventDefault();
-    DeleteListElement();
-}
-
-function DeleteListElement(){
-    textArr.pop();
-    
-    UpdateArrayList();
 }
