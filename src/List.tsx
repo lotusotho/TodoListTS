934285bed328e571932f3a2dd1de5c2450ca7130
Alejandro Ramos <lotusotho11@gmail.com>
2024-02-17 21:31:32 +0100

    Change tasks text

--- src/List.tsx
+++ src/List.tsx
@@ -5,13 +5,13 @@ export function List() {
         <>
             <form action="" onSubmit={handleSumbit}>
                 <input type="text" name="123" id="textInput" className="input-text-field-form"/>
-                <button className="btn-default">Add line</button>
+                <button className="btn-default">Add task</button>
                 <br />
             </form>
             <form action="" onSubmit={DeleteListElementButton}>
                 <CreateStructureList/>
                 <aside>
-                    <button className="btn-default-delete" id="btnDeleteLine" hidden>Delete line</button>
+                    <button className="btn-default-delete" id="btnDeleteLine" hidden>Delete task</button>
                     <br />
                     <br />
                 </aside>
